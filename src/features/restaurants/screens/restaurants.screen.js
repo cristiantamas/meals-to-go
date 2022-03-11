ffeba481239e1e7443d2812ea024f43617075982
Cristian Tamas <cristiantamas05@gmail.com>
2022-03-11 15:51:56 +0200

    Added tab navigation and flat list for restaurants

---
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "styled-components/native";
+import { FlatList } from "react-native";
+import { Searchbar } from "react-native-paper";
+
+import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+import { SafeArea } from "../../../components/utility/safe-area.component";
+
+const SearchContainer = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const RestaurantList = styled(FlatList).attrs({
+  contentContainerStyle: {
+    padding: 16,
+  },
+})``;
+
+export const RestaurantsScreen = () => (
+  <SafeArea>
+    <SearchContainer>
+      <Searchbar placeholder="Search" />
+    </SearchContainer>
+    <RestaurantList
+      data={[{ name: 1 }, { name: 2 }, { name: 3 }, { name: 4 }]}
+      renderItem={() => <RestaurantInfoCard />}
+      keyExtractor={(item) => item.name}
+    />
+  </SafeArea>
+);
