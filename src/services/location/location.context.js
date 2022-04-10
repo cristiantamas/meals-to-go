b1b84780bcb4be56ef55d2fe9f657ecc1eff4ab0
Cristian Tamas <cristiantamas05@gmail.com>
2022-04-10 18:51:24 +0300

    Added LocationContext + search by location + small fix

---
+++ src/services/location/location.context.js
@@ -0,0 +1,50 @@
+import React, { createContext, useEffect, useState } from "react";
+
+import { locationRequest, locationTransform } from "./location.service";
+
+export const LocationContext = createContext();
+
+export const LocationContextProvider = ({ children }) => {
+  const [location, setLocation] = useState([]);
+  const [keyword, setKeyword] = useState("San Francisco");
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const onSearch = (searchKeyword) => {
+    setIsLoading(true);
+    setKeyword(searchKeyword);
+
+    if (!searchKeyword.length) {
+      return;
+    }
+
+    locationRequest(searchKeyword.toLowerCase())
+      .then(locationTransform)
+      .then((result) => {
+        setLocation(result);
+        setIsLoading(false);
+
+        console.log(result);
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        setError(err);
+
+        console.log(err);
+      });
+  };
+
+  return (
+    <LocationContext.Provider
+      value={{
+        isLoading,
+        error,
+        location,
+        search: onSearch,
+        keyword,
+      }}
+    >
+      {children}
+    </LocationContext.Provider>
+  );
+};
