a522f713c9c07ac30b0c5ee0efdde427d7fde6c6
Cristian Tamas <cristiantamas05@gmail.com>
2022-06-21 11:54:35 +0300

    Implemented Favourites

--- App.js
+++ App.js
@@ -1,42 +1,45 @@
-import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import React from "react";
-import { ThemeProvider } from "styled-components";
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar'
+import React from 'react'
+import { ThemeProvider } from 'styled-components'
 
 import {
   useFonts as useOswald,
-  Oswald_400Regular,
-} from "@expo-google-fonts/oswald";
+  Oswald_400Regular
+} from '@expo-google-fonts/oswald'
 
-import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato'
 
-import { theme } from "./src/infrastructure/theme";
-import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
-import { LocationContextProvider } from "./src/services/location/location.context";
-import { Navigation } from "./src/infrastructure/navigation/index";
+import { theme } from './src/infrastructure/theme'
+import { RestaurantsContextProvider } from './src/services/restaurants/restaurants.context'
+import { LocationContextProvider } from './src/services/location/location.context'
+import { FavouritesContextProvider } from './src/services/favourites/favourites.context'
+import { Navigation } from './src/infrastructure/navigation/index'
 
 export default function App() {
   let [oswaldLoaded] = useOswald({
-    Oswald_400Regular,
-  });
+    Oswald_400Regular
+  })
 
   let [latoLoaded] = useLato({
-    Lato_400Regular,
-  });
+    Lato_400Regular
+  })
 
   if (!oswaldLoaded || !latoLoaded) {
-    return null;
+    return null
   }
 
   return (
     <>
       <ThemeProvider theme={theme}>
-        <LocationContextProvider>
-          <RestaurantsContextProvider>
-            <Navigation />
-          </RestaurantsContextProvider>
-        </LocationContextProvider>
+        <FavouritesContextProvider>
+          <LocationContextProvider>
+            <RestaurantsContextProvider>
+              <Navigation />
+            </RestaurantsContextProvider>
+          </LocationContextProvider>
+        </FavouritesContextProvider>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
-  );
+  )
 }
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,37 +1,50 @@
-import React, { useContext } from "react";
-import { ActivityIndicator, Colors } from "react-native-paper";
-import { FlatList, View, TouchableOpacity } from "react-native";
-import styled from "styled-components/native";
+import React, { useContext, useState } from 'react'
+import { ActivityIndicator, Colors } from 'react-native-paper'
+import { FlatList, View, TouchableOpacity } from 'react-native'
+import styled from 'styled-components/native'
 
-import { SafeArea } from "../../../components/utility/safe-area.component";
-import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
-import { Spacer } from "../../../components/spacer/spacer.component";
-import { Search } from "../components/search.component";
+import { SafeArea } from '../../../components/utility/safe-area.component'
+import { FavouritesBar } from '../../../components/favourite/favourites-bar.component'
+import { RestaurantInfoCard } from '../components/restaurant-info-card.component'
+import { Spacer } from '../../../components/spacer/spacer.component'
+import { Search } from '../components/search.component'
 
-import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
+import { RestaurantsContext } from '../../../services/restaurants/restaurants.context'
+import { FavouritesContext } from '../../../services/favourites/favourites.context'
 
 const RestaurantList = styled(FlatList).attrs({
   contentContainerStyle: {
-    padding: 16,
-  },
-})``;
+    padding: 16
+  }
+})``
 
 const Loading = styled(ActivityIndicator)`
   margin-left: -25px;
-`;
+`
 
 const LoadingContainer = styled(View)`
   position: absolute;
   top: 50%;
   left: 50%;
-`;
+`
 
 export const RestaurantsScreen = ({ navigation }) => {
-  const { isLoading, error, restaurants } = useContext(RestaurantsContext);
+  const { isLoading, error, restaurants } = useContext(RestaurantsContext)
+  const { favourites } = useContext(FavouritesContext)
+  const [isToggled, setIsToggled] = useState(false)
 
   return (
     <SafeArea>
-      <Search />
+      <Search
+        isFavouritesToggled={isToggled}
+        onFavouritesToggle={() => setIsToggled(!isToggled)}
+      />
+      {isToggled && (
+        <FavouritesBar
+          favourites={favourites}
+          onNavigate={navigation.navigate}
+        />
+      )}
       {isLoading ? (
         <LoadingContainer>
           <Loading size={50} animating={true} color={Colors.blue300} />
@@ -43,8 +56,8 @@ export const RestaurantsScreen = ({ navigation }) => {
             return (
               <TouchableOpacity
                 onPress={() =>
-                  navigation.navigate("RestaurantDetail", {
-                    restaurant: item,
+                  navigation.navigate('RestaurantDetail', {
+                    restaurant: item
                   })
                 }
               >
@@ -52,11 +65,11 @@ export const RestaurantsScreen = ({ navigation }) => {
                   <RestaurantInfoCard restaurant={item} />
                 </Spacer>
               </TouchableOpacity>
-            );
+            )
           }}
           keyExtractor={(item) => item.name}
         />
       )}
     </SafeArea>
-  );
-};
+  )
+}
